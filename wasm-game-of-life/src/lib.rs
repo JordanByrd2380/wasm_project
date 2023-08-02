633b7ee77e69a0341ae5857597d175d3a85a68d7
Jordan <jbyrd23@stanford.edu>
2023-08-02 16:38:06 -0500

    Addded console.log, injection, and fib

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,13 +1,43 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+//use web_sys::console;
+
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+extern "C" {
+    // This allows us to call JavaScript's console.log() from Rust
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn log_to_console(message: &str) {
+    log(message);
+}
+
+#[wasm_bindgen]
+pub fn greet(greeting: &str) {
+    alert(&format!("Hello, {}!", greeting));
+}
+
+#[wasm_bindgen]
+pub fn fib(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    if n < 0 {
+        return -1;
+    }
+    
+    return fib(n - 1) + fib(n - 2);
 }
