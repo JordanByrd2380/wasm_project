a8dc28d3a46d95ec607944a047ff5657db79d310
Jordan <jbyrd23@stanford.edu>
2023-08-09 01:57:14 -0500

    Adding eval, createElement, addEventListener

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,21 +1,59 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-//use web_sys::console;
+use web_sys::Event;
+use web_sys::HtmlInputElement;
+use web_sys::HtmlTextAreaElement;
+use js_sys::eval;
+
 
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
-}
 
-
-#[wasm_bindgen]
-extern "C" {
     // This allows us to call JavaScript's console.log() from Rust
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
+
 }
+
+#[wasm_bindgen]
+pub fn eval_wasm(js_code: &str) -> Result<JsValue, JsValue> {
+    eval(js_code)
+}
+
+#[wasm_bindgen]
+pub fn create_element(element_name: &str) {
+    let document = web_sys::window().unwrap().document().unwrap();
+    document.create_element(element_name);
+}
+
+#[wasm_bindgen]
+pub fn add_event_listener(element_id: &str) {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let input_element = document
+        .get_element_by_id(element_id)
+        .unwrap()
+        .dyn_into::<HtmlInputElement>()
+        .unwrap();
+
+
+    let closure = Closure::wrap(Box::new(|e: Event| {
+        e.current_target()
+            .unwrap()
+            .dyn_into::<HtmlTextAreaElement>()
+            .unwrap();
+
+    }) as Box<dyn FnMut(_)>);
+
+    input_element.add_event_listener_with_callback(element_id, &closure.as_ref().unchecked_ref());
+
+    closure.forget();
+
+}
+
+
 
 #[wasm_bindgen]
 pub fn log_to_console(message: &str) {
